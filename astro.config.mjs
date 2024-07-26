9098763174c80e258a442a96959ff24db47c7a9a
Eugen Preglej <ep53583@fer.hr>
2024-07-26 23:50:30 +0200

    Empty project initialized.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,4 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
-    output: 'hybrid',
-    adapter: netlify()
-});
+export default defineConfig({});
