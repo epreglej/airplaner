c8fe0679d8e81e7a7b9392b7af9738658bdbb05d
Eugen Preglej <ep53583@fer.hr>
2024-08-02 14:06:21 +0200

    Reverted to older version of TailwindCSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {
-        enabled: false
-    },
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+  ],
 });
